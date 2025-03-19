d56ac74e4451a84abcb13da1c0661c2d3fdde150
Ahmed Shouman <amgshouman@gmail.com>
2025-03-19 03:48:17 +0200

    [UI-879] fix(api): send an email to the customer when a producta

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,3 +1,7 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+
 module.exports = {
   branches: ["main"],
   plugins: [
@@ -29,13 +33,44 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "conventionalcommits",
+        parserOpts: {
+          headerPattern: "^\\[UI-\\d+\\] (\\w+)(?:\\(([^)]+)\\))?(!)?: (.*)$",
+          headerCorrespondence: ["type", "scope", "breaking", "subject"],
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+        },
+        releaseRules: [
+          { type: "feat", breaking: true, release: "major" },
+          { type: "fix", breaking: true, release: "major" },
+          { breaking: true, release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "revert", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "docs", release: false },
+          { type: "refactor", release: false },
+          { type: "test", release: false },
+          { type: "build", release: false },
+          { type: "ci", release: false },
+          { type: "chore", release: false },
+        ],
+        writerOpts: {
+          header: function (context) {
+            const date = new Date().toISOString().split("T")[0];
+            return `# 🚀 Release v${context.version} - ${date} 🎉\n\n`;
+          },
+          commitsSort: ["subject", "scope"],
+        },
         presetConfig: {
           types: [
+            { mess: "feat", section: "💥 Breaking Changes", hidden: false },
             { type: "feat", section: "🚀 Features", hidden: false },
             { type: "fix", section: "🐛 Bug Fixes", hidden: false },
             { type: "revert", section: "⏪ Reverts", hidden: false },
-            { type: "perf", section: "⚡ Performance Improvements", hidden: false },
+            {
+              type: "perf",
+              section: "⚡ Performance Improvements",
+              hidden: false,
+            },
             { type: "docs", section: "📚 Documentation", hidden: false },
             { type: "refactor", section: "🛠 Code Refactoring", hidden: false },
             { type: "test", section: "✅ Tests", hidden: true },
